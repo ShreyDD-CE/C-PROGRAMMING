1c36e8bf72168d5831e2fb8a6cce80f020b986ae
Shrey Dharmesh Dhameliya <shreycomputer.engineer@gmail.com>
2025-07-21 14:18:54 +0530

    Add files via upload
    Computer Lab practical CE 1st year

---
+++ PROJECT 3/Shrey area and peri.c
@@ -0,0 +1,15 @@
+
+
+
+#include<stdio.h>
+int main()
+{
+     int length=70;
+     int breadth=90;
+     int area=length*breadth;
+     int perimeter=2*length+2*breadth;
+     printf("Area of the plot is\t:%d",area);
+     printf("\nPerimeter of the plot is\t:%d",perimeter);
+     printf("\nThis program is created by SHREY_25TCEZMD\t");
+     return 0;
+}
---
+++ PROJECT 3/shrey cb.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int Num;
+    //Num=10;
+    printf("Enter the Number: ");
+    scanf("%d",&Num);
+    printf("This is number %d",Num);
+    return 0;
+}
