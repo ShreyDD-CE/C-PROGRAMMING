fe86b7f8cd6f6b37167ff5f4626cf88e3606e5f2
Shrey Dharmesh Dhameliya <shreycomputer.engineer@gmail.com>
2025-09-08 16:09:31 +0530

    Add files via upload

---
+++ Project 20/PR 20.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+
+{
+    int j,i,l,counter=0;
+    int id[10]={1,2,3,4,5,6,7,8,9,10},ID[10];
+    for(l=0;l<10;l++)
+    {
+    printf("Enter ID :");
+    scanf("%d",&ID[l]);
+    }
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++)
+        {
+            if(ID[i]==id[j])
+            {
+                counter++;
+            }
+        }
+    }
+    if(counter==10)
+    {
+        printf("\n\nAll the values exist");
+    }
+    else
+    {
+        printf("\n\n%d values are missing",10-counter);
+    }
+}
