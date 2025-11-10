1ccf22d2d1964607ae58ba0d59c0a19cf6a833cf
Shrey Dharmesh Dhameliya <shreycomputer.engineer@gmail.com>
2025-11-10 14:46:33 +0530

    Add files via upload

---
+++ Project 7/Ticket.c
@@ -0,0 +1,18 @@
+s#include<stdio.h>
+
+void main()
+{
+    int Age;
+    printf("Enter You Age (In Years):");
+    scanf("%d",&Age);
+
+    if(Age>12)
+    {
+        printf("\nYou Would Be Charged 100/- as Ticket Fare");
+    }
+   else
+   {
+       printf("\nYou Are Allowed Free Entry");
+   }
+    printf("\n\n\nThis Program was developed by Shrey Dharmesh Dhameliya_25CE024_ ");
+}
