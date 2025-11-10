089d87d75b41383ce8b040920ec762627fafe827
Shrey Dharmesh Dhameliya <shreycomputer.engineer@gmail.com>
2025-11-10 15:04:20 +0530

    Add files via upload

---
+++ Project 8/Bank.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+
+{
+    int Age;
+    printf("Enter Your Age (In Years):");
+    scanf("%d",&Age);
+
+    if(Age>=18)
+    {
+        printf("\nYou Open An Account In Our Bank");
+    }
+    else
+    {
+        printf("\nPlease Try after you reach the eligible Age");
+    }
+    printf("\n\n\nThis Program was developed by Shrey Dharmesh Dhameliya_25CE024_ ");
+}
+
