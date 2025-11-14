76cafd1e67853ae24338a38eae3fad5e8391b457
Shrey Dharmesh Dhameliya <shreycomputer.engineer@gmail.com>
2025-11-14 15:42:33 +0530

    Add files via upload

---
+++ Project 14/Bankbalance.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void main()
+{
+    float Bank_Balance=5000;
+    float Withdrawal_amount;
+
+    do
+    {
+        printf("\nEnter The Withrawal amount:");
+        scanf("%f",&Withdrawal_amount);
+
+        if (Withdrawal_amount > Bank_Balance)
+            {
+            printf("Insufficient balance! You have only %.2f\n", Bank_Balance);
+            break;
+        }
+        else
+            {
+            Bank_Balance -= Withdrawal_amount;
+            printf("The Current bank balance is: %.2f\n\n", Bank_Balance);
+        }
+
+        printf("\n\n");
+
+    }while( Bank_Balance>0);
+
+    printf("The Bank Balance is insufficient For The Entered amount");
+}
